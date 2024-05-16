8aed6a002741682688e1107dabd1d86786aebdfa
JavaJedi <aleksk170400@gmail.com>
2024-05-17 00:55:56 +0500

    Initial commit

--- src/PersonBuilder.java
+++ src/PersonBuilder.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class PersonBuilder {
+public class PersonBuilder {
+    private String name;
+    private String surname;
+    private int age = -1;
+    private String address;
+
+    public PersonBuilder setName(String name) throws IllegalArgumentException {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Не указано имя!");
+        } else this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) throws IllegalArgumentException {
+        if (surname == null || surname.isEmpty()) {
+            throw new IllegalArgumentException("Не указана фамилия!");
+        } else this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) throws IllegalArgumentException {
+        if (age < 0) {
+            throw new IllegalArgumentException("Возраст не может быть меньше 0!");
+        } else this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() throws IllegalStateException {
+        Person person;
+        if (name == null || surname == null)
+            throw new IllegalStateException("Не указаны имя и фамилия!");
+        if (age < 0) {
+            person = new Person(name, surname);
+        } else person = new Person(name, surname, age);
+        person.setAddress(address);
+        return person;
+    }
 }
